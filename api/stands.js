d57df06d07c1c94f4bc4ba460163e05509ae22c7
Mehluli Hikwa <mehlulihikwa@gmail.com>
2024-06-04 10:15:35 +0200

    Update API Fecth function

--- api/stands.js
+++ api/stands.js
@@ -1,36 +1,4 @@
-// fetch('./api/stands.json')
-// 	.then(response => response.json())
-// 	.then(data => {
-// 		let standsModals = document.getElementById('standsModals');
-// 		// let standsModals = document.querySelector('body');
-// 		data.forEach(stand => {
-// 			let standModalItem = document.createElement('div');
-// 			standModalItem.innerHTML = `
-// 			<div class="modal micromodal-slide" id="${modal.ID}" aria-hidden="true">
-// 				<div class="modal__overlay" tabindex="-${modal.ID}" data-micromodal-close>
-// 					<div class="modal__container" role="dialog" aria-modal="true">
-// 						<header class="modal__header">
-// 							<h2 class="modal__title ${modal.availability}">Stand No. ${modal.ID}</h2>
-// 							<button aria-lable="Clode Modal" class="modal__close" data-micromodal-close></button>
-// 						</header>
-// 						<main class="modal__content">
-// 							<h4>Stand Size: ${modal.sqm}</h4>
-// 							<p>${modal.description}</p>
-// 						</main>
-// 						<footer class="modal__footer">
-// 							<a class="modal__btn" href="https://thatafro.netlify.app" target="_blank">Buy Stand</a>
-// 							<a class="modal__btn" data-micromodal-close>Close</a>
-// 						</footer>
-// 					</div>
-// 				</div>
-// 			</div>`;
-// 			standsModals.appendChild(standModalItem);
-// 		});
-// 	})
-// 	.catch(error => console.error('Error fetching stands:', error));
-
-
-
+// Sample API endpoint returning an array of modal data
 const apiEndpoint = './api/stands.json';
 
 // Fetch modal data from apiEndpoint
