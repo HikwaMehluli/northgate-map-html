883ef4aad02c84d5350bb88474c163afec7a9e85
Mehluli Hikwa <mehlulihikwa@gmail.com>
2024-06-14 21:45:16 +0200

    Upgrade entire Project: Icon Font, Colors and Scripts

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 import tippy from 'tippy.js';
 import MicroModal from 'micromodal';
-
-// The two scripts below are imported from /js folder
-import svgPanZoom from'./svg-pan-zoom.min.js';
+import svgPanZoom from './svg-pan-zoom.min.js';
 import './hammer.js';
 
-
-window.addEventListener('load', function() {
-	console.log('Page fully loaded');
-
-
+window.addEventListener('load', function () {
+	console.log('%c Made by Digital Afros', 'font-size: 12px;');
 	
+
 	// 
 	// TippyJS, Tooltip Options - https://atomiks.github.io/tippyjs/v6/customization/
 	//
@@ -19,21 +15,120 @@ window.addEventListener('load', function() {
 	function isMobileDevice() {
 		return /Mobi|Android/i.test(navigator.userAgent);
 	}
-	
+
 	// Initialize Tippy.js and remove on mobile devices
 	if (!isMobileDevice()) {
-        tippy('[data-tippy-content]', {
+		tippy('[data-tippy-content]', {
 			arrow: true,
 			delay: [100, 100],
 		});
-    }
-	
+	}
+
 
 
 	// 
 	// MicroModal - https://micromodal.vercel.app/#usage
 	//
-	MicroModal.init();
+	// MicroModal.init();
+
+	// Get sample API endpoint
+	const apiEndpoint = './api/stands.json';
+
+	// Fetch modal data from apiEndpoint
+	async function fetchModalData() {
+		try {
+			const response = await fetch(apiEndpoint);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.log('Error fetching modal data Mr Hikwa:', error);
+			return [];
+		}
+	}
+
+	// Generate Modal HTML Structure
+	function createModal(modal) {
+		return `
+		<div class="modal micromodal-slide" id="${modal.ID}" aria-hidden="true">
+			<div class="modal__overlay" tabindex="-${modal.ID}" data-micromodal-close>
+				<div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="${modal.ID}-title">
+					<header class="modal__header">
+						<h2 class="modal__title ${modal.availability}" id="${modal.ID}-title">
+							Stand Number: ${modal.ID}
+						</h2>
+						<h3 class="modal__sqm">
+							Stand size: ${modal.sqm}sqm
+						</h3>
+						<button aria-label="Close Modal" class="modal__close" data-micromodal-close></button>
+					</header>
+					<main class="modal__content">
+						<h3 class="modal__price">
+							USD${modal.price.toLocaleString('en-US', { //  converts a number into a string using a specific locale
+								style: 'currency', // Specifies that the number should be formatted as currency.
+								currency: 'USD', // Specifies the currency to be USD (US Dollar).
+								minimumFractionDigits: 2, // Ensures that at least 2 decimal places are displayed.
+								maximumFractionDigits: 2, // Ensures that at most 2 decimal places are displayed.
+							})}
+						</h3>
+						<p>${modal.description}</p>
+					</main>
+					<footer class="modal__footer">
+						<a class="modal__btn" href="https://staging.northgate.co.zw">Buy Stand</a>
+						<a class="modal__btn" data-micromodal-close>Close</a>
+					</footer>
+				</div>
+			</div>
+		</div>`;
+	}
+
+	// Append Modals to the DOM
+	async function loadModals() {
+		const modalContainer = document.getElementById('modal-container');
+		const modals = await fetchModalData();
+
+		modals.forEach(modal => {
+			modalContainer.insertAdjacentHTML('beforeend', createModal(modal));
+		});
+
+		// Initialize MicroModal
+		MicroModal.init();
+	}
+	// Call the function to load modals on page load
+	loadModals();
+
+
+
+	// 
+	// Function to update HTML elements on SVG's
+	// 
+	async function updateElements() {
+		const svgData = await fetchModalData();
+
+		svgData.forEach(item => {
+			// Select the corresponding <g> element
+			const gElement = document.getElementById(`_${item.ID}`);
+
+			if (gElement) {
+				// Update data-micromodal-trigger attribute
+				gElement.setAttribute('data-micromodal-trigger', item.ID);
+
+				// Update the class of the polygon element for availability status
+				const polygon = gElement.querySelector('polygon');
+				if (polygon) {
+					// polygon.className.baseVal = item.availability;
+					polygon.setAttribute('class', item.availability);
+				}
+
+				// Update the text content
+				const textElement = gElement.querySelector('text');
+				if (textElement) {
+					textElement.textContent = item.ID; // Set text to ID
+				}
+			}
+		});
+	}
+	// Call the function to update elements
+	updateElements();
 
 
 
@@ -41,7 +136,7 @@ window.addEventListener('load', function() {
 	// SVG Pan Zoom - https://github.com/bumbu/svg-pan-zoom
 	//
 	const svgElement = document.querySelector('svg');
-	const svgPanZoomInstance = svgPanZoom(svgElement, {
+	svgPanZoom(svgElement, {
 		viewportSelector: '.svg-pan-zoom_viewport',
 		panEnabled: true,
 		controlIconsEnabled: false,
@@ -70,16 +165,14 @@ window.addEventListener('load', function() {
 					initialScale = 3, // Initial zoom level = 2
 					pannedX = 0,
 					pannedY = 0
-					
 
 				// Set initial zoom level
 				instance.zoom(initialScale);
 
 				// set initial pan area
 				if (!isMobileDevice()) {
-					instance.pan({x: 200, y: -1750});
+					instance.pan({ x: 200, y: -1750 });
 				}
-				
 
 
 				// 
@@ -101,10 +194,6 @@ window.addEventListener('load', function() {
 				// 	instance.panBy({x: 100, y: 0});
 				// });
 
-				// document.getElementById('reset').addEventListener('click', function () {
-				// 	instance.reset();
-				// });
-
 				document.getElementById('zoom-in').addEventListener('click', function () {
 					instance.zoomIn();
 				});
@@ -113,7 +202,6 @@ window.addEventListener('load', function() {
 					instance.zoomOut();
 				});
 
-				
 
 				// 
 				// Init Hammer for Touch Controls
@@ -122,14 +210,6 @@ window.addEventListener('load', function() {
 				this.hammer = Hammer(options.svgElement, {
 					inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
 				})
-
-				// Enable pinch
-				this.hammer.get('pinch').set({ enable: true })
-
-				// // Handle double tap
-				// this.hammer.on('doubletap', function (ev) {
-				// 	instance.zoomIn()
-				// })
 
 				// Handle pan
 				this.hammer.on('panstart panmove', function (ev) {
@@ -145,6 +225,9 @@ window.addEventListener('load', function() {
 					pannedY = ev.deltaY
 				})
 
+				// Enable pinch
+				this.hammer.get('pinch').set({ enable: true })
+
 				// Handle pinch
 				this.hammer.on('pinchstart pinchmove', function (ev) {
 					// On pinch start remember initial zoom
@@ -152,7 +235,6 @@ window.addEventListener('load', function() {
 						initialScale = instance.getZoom()
 						instance.zoomAtPoint(initialScale * ev.scale, { x: ev.center.x, y: ev.center.y })
 					}
-
 					instance.zoomAtPoint(initialScale * ev.scale, { x: ev.center.x, y: ev.center.y })
 				})
 
@@ -165,10 +247,10 @@ window.addEventListener('load', function() {
 				this.hammer.destroy()
 			},
 
-			click: function(event, instance) {
+			click: function (event, instance) {
 				// Prevent default click behavior if panning or zooming
 				if (instance.getZoom() !== 1 || instance.getPan().x !== 0 || instance.getPan().y !== 0) {
-				event.preventDefault();
+					event.preventDefault();
 				}
 			}
 		}
