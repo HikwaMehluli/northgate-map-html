3933b4f692d6748c2eda7535f49d5e06b19f6575
Mehluli Hikwa <mehlulihikwa@gmail.com>
2024-10-23 07:47:54 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ window.addEventListener('load', function () {
 	//
 
 	// Get API endpoint
-	const apiEndpoint = './api/phasetwo.json';
+	const apiEndpoint = './api/stands.json';
 
 	// Fetch modal data from apiEndpoint
 	async function fetchModalData() {
@@ -41,7 +41,7 @@ window.addEventListener('load', function () {
 	// Generate Modal HTML Structure
 	function createModal(modal) {
 		return `
-		<div class="modal micromodal-slide" id="res_${modal.ID}" aria-hidden="true">
+		<div class="modal micromodal-slide" id="${modal.ID}" aria-hidden="true">
 			<div class="modal__overlay" tabindex="-${modal.ID}" data-micromodal-close>
 				<div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="${modal.ID}-title">
 					<header class="modal__header">
@@ -65,7 +65,7 @@ window.addEventListener('load', function () {
 						<p>${modal.description}</p>
 					</main>
 					<footer class="modal__footer">
-						<a class="modal__btn" href="https://www.northgateestates.co.zw/">Buy Stand</a>
+						<a class="modal__btn" href="https://staging.northgate.co.zw">Buy Stand</a>
 						<a class="modal__btn" data-micromodal-close>Close</a>
 					</footer>
 				</div>
@@ -98,12 +98,11 @@ window.addEventListener('load', function () {
 
 		svgData.forEach(item => {
 			// Select the corresponding <g> element
-			// const gElement = document.getElementById(`_${item.ID}`);// phase_one
-			const gElement = document.getElementById(`res_${item.ID}`);// phase_two
+			const gElement = document.getElementById(`_${item.ID}`);
 
 			if (gElement) {
 				// Update data-micromodal-trigger attribute
-				gElement.setAttribute('data-micromodal-trigger', item.ID); 
+				gElement.setAttribute('data-micromodal-trigger', item.ID);
 
 				// Update the class of the polygon element for availability status
 				const polygon = gElement.querySelector('polygon');
@@ -167,7 +166,7 @@ window.addEventListener('load', function () {
 				// Variables
 				// 
 				var instance = options.instance,
-					initialScale = 1, // Initial zoom level phase_one = 3
+					initialScale = 3, // Initial zoom level = 2
 					pannedX = 0,
 					pannedY = 0
 
@@ -175,9 +174,9 @@ window.addEventListener('load', function () {
 				instance.zoom(initialScale);
 
 				// Set initial pan
-				// if (!isMobileDevice()) {
-				// 	instance.pan({ x: 200, y: -1765 });
-				// }
+				if (!isMobileDevice()) {
+					instance.pan({ x: 200, y: -1765 });
+				}
 
 
 				// 
